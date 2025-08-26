de080fff188735121c0d3464af070c1eee20a716
Guillermo Valarezo Guzman <gvalarezog@gmail.com>
2025-08-26 18:42:16 -0500

    commit inical

---
+++ variables.py
@@ -0,0 +1,11 @@
+nombre = input("Ingresa tu nombre: ")
+edad = 25
+nombre = 'Carlos'
+es_VIP = True
+sueldo = 500.30
+hobbies = ['Programar', 'Ver TV', 'Jugar futbol']
+
+print(f'Hola mi nombre es: {nombre}, mi edad: {edad}')
+
+print(type(edad))
+print(type(sueldo))
