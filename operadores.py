4f32c617176fa427b72af24ea50931e3d87d0fc0
Guillermo Valarezo Guzman <gvalarezog@gmail.com>
2025-08-26 20:14:00 -0500

    se agrega el archivo operadores.py y se agrego ejemplos en varibles

---
+++ operadores.py
@@ -0,0 +1,24 @@
+a = 5
+b = 2
+r = a + b
+print(f'Resultado: {r}')
+r = a - b
+print(f'Resultado: {r}')
+r = a * b
+print(f'Resultado: {r}')
+r = a / b
+print(f'Resultado: {r}')
+r = a % b
+print(f'Resultado: {r}')
+r = a // b
+print(f'Resultado: {r}')
+r = a ** b
+print(f'Resultado: {r}')
+
+contador = 0
+# contador = contador + 1
+contador += 2
+print(f'Resultado: {contador}')
+
+h = 5==6
+print(f'Resultado: {h}')
--- variables.py
+++ variables.py
@@ -1,11 +1,92 @@
-nombre = input("Ingresa tu nombre: ")
-edad = 25
-nombre = 'Carlos'
-es_VIP = True
-sueldo = 500.30
-hobbies = ['Programar', 'Ver TV', 'Jugar futbol']
+# nombre = input("Ingresa tu nombre: ")
+# edad = 25
+# nombre = 'Carlos'
+# es_VIP = True
+# sueldo = 500.30
+# hobbies = ['Programar', 'Ver TV', 'Jugar futbol']
+#
+# print(f'Hola mi nombre es: {nombre}, mi edad: {edad}')
+#
+# print(f'Tipo de dato de variable edad: {type(edad)}')
+# print(type(sueldo))
+# print(type(hobbies))
+# print(type(nombre))
+#
+# edad = 'treinta'
+# print(f'Tipo de dato de variable edad: {type(edad)}')
+#
+#
+#
+#
+# x = int(1)   # x will be 1
+# y = int(2.8) # y will be 2
+# z = int("3") # z will be 3
+#
+#
+# print(x)
+# print(type(x))
+# print(y)
+# print(type(y))
+# print(z)
+# print(type(z))
+#
+#
+# print(type("Hello"))
+# print(type('Hello'))
 
-print(f'Hola mi nombre es: {nombre}, mi edad: {edad}')
 
-print(type(edad))
-print(type(sueldo))
+# a = "Hello, World!"
+# print(a)
+# print(a[0])
+# print(a[-1])
+# print(a[5])
+#
+#
+# for x in "banana":
+#   print(x)
+#
+# r = 'alsdjfkljaskldfjiwuruwe89723874skjafklj'
+# print(len(r))
+# print(len('casa'))
+#
+# txt = "The best things in life are free!"
+# print("best" not in txt)
+#
+# if "free " in txt:
+#     print("Si se encuentra free en el texto")
+# else:
+#     print("No se encuentra free en el texto")
+#
+# b = "Hello, World!"
+# print(b[2:5])
+# print(type(b[2:5]))
+#
+# nombre = input("Nombre: ")
+# print(nombre.upper())
+# print(nombre.lower())
+# print(nombre.capitalize())
+#
+# q = input("apellido: ")
+# print(q.capitalize().strip())
+
+
+price = 59
+txt = f"The price is {20 * price:.2f} \ndollars"
+
+print(txt)
+
+bandera = True
+b2 = False
+
+
+print(b2)
+
+print(bool("abc"))
+print(bool(123))
+print(bool(["apple", "cherry", "banana"]))
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
